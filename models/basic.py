1d5b2b36c5f3764c5fd6b8dd13fd095780250749
joshua.wood@btinternet.com <joshua.wood@btinternet.com>
2020-10-12 14:43:03 +0100

    added test data and functionalty to read it into memory for training

--- models/basic.py
+++ models/basic.py
@@ -3,6 +3,18 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+from pathlib import Path
+
+#create dataset from text files
+path = Path(__file__).parent / 'test data'
+dataset = keras.preprocessing.text_dataset_from_directory(path)
+
+for data, labels in dataset:
+   print(data.shape)  # (64,)
+   print(data.dtype)  # string
+   print(labels.shape)  # (64,)
+   print(labels.dtype)  # int32
+"""
 # Define Sequential model with 3 layers
 model = keras.Sequential(
     [
@@ -14,4 +26,4 @@ model = keras.Sequential(
 # Call model on a test input
 x = tf.ones((3, 3))
 y = model(x)
-print(model.layers)
+print(model.layers) """
