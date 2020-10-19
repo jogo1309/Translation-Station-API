a16a0f1efce133fa24c61315c1f289126f4eb149
joshua.wood@btinternet.com <joshua.wood@btinternet.com>
2020-10-19 15:59:02 +0100

    added logic to tokenize data set

--- models/basic.py
+++ models/basic.py
@@ -5,15 +5,34 @@ from tensorflow.keras import layers
 
 from pathlib import Path
 
+def getDataset(dSet):
+    for tensor in dSet:
+        return tensor[0]
+
 #create dataset from text files
 path = Path(__file__).parent / 'test data'
 dataset = keras.preprocessing.text_dataset_from_directory(path)
 
-for data, labels in dataset:
-   print(data.shape)  # (64,)
-   print(data.dtype)  # string
-   print(labels.shape)  # (64,)
-   print(labels.dtype)  # int32
+#create text vectorizer to create mapping between datasets
+text_vectorizer = layers.experimental.preprocessing.TextVectorization(output_mode ="int")
+
+print(dataset)
+
+initalDataSet = getDataset(dataset)
+print(initalDataSet)
+#adapt the vectorizer and appliy it to the data to create mapping from en to fr
+text_vectorizer.adapt(initalDataSet)
+
+dataset= dataset.map(
+    lambda x, y: (text_vectorizer(x), y)
+)
+#get mapped data set
+mappedDataSet = getDataset(dataset)
+print(mappedDataSet)
+
+
+
+
 """
 # Define Sequential model with 3 layers
 model = keras.Sequential(
@@ -26,4 +45,14 @@ model = keras.Sequential(
 # Call model on a test input
 x = tf.ones((3, 3))
 y = model(x)
-print(model.layers) """
+print(model.layers) 
+
+for dSet in dataset:
+    text_vectorizer.adapt(dSet[0])
+    dataset= dataset.map(
+        lambda x, y: (text_vectorizer(x), y)
+    )
+for dSet in dataset:
+    print(dSet[0])
+
+"""
