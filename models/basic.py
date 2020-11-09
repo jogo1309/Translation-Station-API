a767700409818d5e0c4b8eeae1d0e9dd3de91855
joshua.wood@btinternet.com <joshua.wood@btinternet.com>
2020-11-09 14:55:21 +0000

    created pre-processing functions and apply to EN and FR datasets

--- models/basic.py
+++ models/basic.py
@@ -6,31 +6,36 @@ from tensorflow.keras import layers
 from pathlib import Path
 import string
 
-#get data line by line
+#get data line by line and process it
+def pre_process_data(path, is_output):
+    unique_sentances = []
+    unique_words = set()
+    f = open(path, encoding="utf8")
+    for line in f.readlines():
+        #convert to lower case, strip punctuation, remove newlines and add to sentance array
+        line = line.lower().translate(str.maketrans('', '', string.punctuation)).replace("\n", "")
+        if(is_output):
+            #add start and end tokens to translations
+            line = "[[" + line + "]]"
+        unique_sentances.append(line)
+        print(line)
+        #add to unique words found
+        words = line.split(" ")
+        for word in words:
+            #print(word)
+            if word not in unique_words:
+                unique_words.add(word)
+    
+    return unique_sentances, sorted(list(unique_words))
+#english dataset
 path = Path(__file__).parent  / 'test data'  / 'europarl-v7-EN.txt'
+input_sentances, input_words = pre_process_data(path, False)
+print(input_words)
 
-f = open(path, encoding="utf8")
-
-input_sentances = []
-output_sentances = []
-
-input_words = set()
-output_words = set()
-
-
-for line in f.readlines():
-    #convert to lower case, strip punctuation, remove newlines and add to sentance array
-    line = line.lower().translate(str.maketrans('', '', string.punctuation)).replace("\n", "")
-    input_sentances.append(line)
-    print(line)
-    #add to unique words found
-    words = line.split(" ")
-    for word in words:
-        #print(word)
-        if word not in input_words:
-            input_words.add(word)
-
-print(sorted(list(input_words)))
+#french dataset
+path = Path(__file__).parent  / 'test data'  / 'europarl-v7-FR.txt'
+output_sentances, output_words = pre_process_data(path, True)
+print(output_words)
 
     
 
