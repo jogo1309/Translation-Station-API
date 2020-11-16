ac28be06b20d8d296cba87c5324eb6675ef1e48a
joshua.wood@btinternet.com <joshua.wood@btinternet.com>
2020-11-16 15:56:05 +0000

    created empty one hot vectors from datasets

--- models/en_2_fr.py
+++ models/en_2_fr.py
@@ -1,4 +1,4 @@
-import numpy as numpy
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -27,9 +27,19 @@ output_sentances, output_words, num_decoder_tokens, max_output_length = pre_proc
 
 #word dictionaries
 input_word_index = dict([(word, i) for i, word in enumerate(input_words)])
-output_word_index = dict([word , i] for i,word in enumerate(output_words))
-    
+output_word_index = dict([(word , i) for i,word in enumerate(output_words)])
 
+#one hot encoding array's
+#each array has 3 dimentions 1: amount of sentances, 2: max length of a sentance, 3: amount of unique words in the dataset
+
+#empty vectors of correct length
+encoder_input_vectors = np.zeros((len(input_sentances), max_input_length, num_encoder_tokens), dtype='float32')
+decoder_input_vectors = np.zeros((len(output_sentances), max_output_length, num_decoder_tokens), dtype='float32')
+decoder_output_vectors = np.zeros((len(output_sentances), max_output_length, num_decoder_tokens), dtype='float32')
+
+#create one hot encoding
+
+print(encoder_input_vectors)
 
 """
 # Define Sequential model with 3 layers
