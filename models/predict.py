95d8f9ca68bd3cb8c44d7591689f19ddc7b76267
joshua.wood@btinternet.com <joshua.wood@btinternet.com>
2020-12-28 14:14:43 +0000

    added gru prediction

--- models/predict.py
+++ models/predict.py
@@ -13,12 +13,15 @@ def predict(input_word_index, output_word_index, max_length, sentance):
     encoder_inputs = model_fr.input[0]
     encoder_outputs, encoder_state_h, encoder_state_c = model_fr.layers[4].output
     encoder_states = [encoder_state_h, encoder_state_c]
+    #encoder_outputs, encoder_state_h = model_fr.layers[4].output
+    #encoder_states = [encoder_state_h]
     encoder_model = keras.Model(encoder_inputs, encoder_states)
 
     #decoder setup
     decoder_state_h = keras.Input(shape=(latent_dim,), name="stateh")
     decoder_state_c = keras.Input(shape=(latent_dim,), name="statec")
     decoder_state = [decoder_state_h, decoder_state_c]
+    #decoder_state = [decoder_state_h]
 
     #embeddings of decoder seq
     decoder_inputs = model_fr.input[1]
@@ -29,6 +32,9 @@ def predict(input_word_index, output_word_index, max_length, sentance):
     decoder_lstm = model_fr.layers[5]
     inf_decoder_outputs , inf_state_h, inf_state_c = decoder_lstm(inf_decoder_embed, initial_state=decoder_state)
     inf_decoder_states = [inf_state_h, inf_state_c]
+    #decoder_gru = model_fr.layers[5]
+    #inf_decoder_outputs , inf_state_h = decoder_gru(inf_decoder_embed, initial_state=decoder_state)
+    #inf_decoder_states = [inf_state_h]
 
     #pass output through dense layer
     decoder_dense = model_fr.layers[6]
@@ -52,6 +58,7 @@ def predict(input_word_index, output_word_index, max_length, sentance):
 
         while not should_stop:
             output_tokens, h, c = inf_decoder_model.predict([frn_sentance] + encoded_state)
+            #output_tokens, h = inf_decoder_model.predict([frn_sentance] + [encoded_state])
             found_word_index = np.argmax(output_tokens[0, -1, :])
             found_word = list(output_word_index.keys())[list(output_word_index.values()).index(found_word_index)]
             output_sentance += ' ' + found_word
@@ -65,6 +72,7 @@ def predict(input_word_index, output_word_index, max_length, sentance):
             frn_sentance[0,0] = found_word_index
 
             encoded_state = [h, c]
+            #encoded_state = [h]
         return output_sentance.replace(']','')
     #test prediction
     print(max_length)
