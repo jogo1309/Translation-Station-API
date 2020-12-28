c2883af54976dc4524d357a7ac50ea05b1013909
joshua.wood@btinternet.com <joshua.wood@btinternet.com>
2020-12-28 14:14:27 +0000

    added GRU model

--- models/en_2_fr.py
+++ models/en_2_fr.py
@@ -29,12 +29,16 @@ def run_model():
     encoder_lstm = layers.LSTM(latent_dim, return_state=True)
     encoder_outputs, state_h, state_c = encoder_lstm(encoder_embed)
     encoder_states = [state_h, state_c]
+   # encoder_gru = layers.GRU(latent_dim, return_state=True)
+    #encoder_outputs, state_h = encoder_gru(encoder_embed)
 
     #set up decoder
     decoder_inputs = keras.Input(shape=(None,))
     decoder_embed = layers.Embedding(num_decoder_tokens, latent_dim, mask_zero=True)(decoder_inputs)
     decoder_lstm = layers.LSTM(latent_dim, return_sequences=True, return_state=True)
     decoder_outputs, _, _  = decoder_lstm(decoder_embed, initial_state=encoder_states)
+    #decoder_gru = layers.GRU(latent_dim, return_sequences=True, return_state=True)
+    #decoder_outputs, _ = decoder_gru(decoder_embed, initial_state=state_h)
     decoder_dense = layers.Dense(num_decoder_tokens, activation='softmax')
     decoder_outputs= decoder_dense(decoder_outputs)
 
