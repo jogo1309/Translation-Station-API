ac6f005bdb1173c16fc63399336515864a2033bb
joshua.wood@btinternet.com <joshua.wood@btinternet.com>
2021-01-11 11:24:38 +0000

    cleaned up how different models are defined

--- models/en_2_fr.py
+++ models/en_2_fr.py
@@ -16,63 +16,96 @@ encoder_input_vector, decoder_input_vector, decoder_output_vector, num_encoder_t
 
 #print(encoder_input_vector[1,12])
 
-def run_model():
+def LSTM_model(latent_dim):
+    #set up encoder
+    encoder_inputs = keras.Input(shape=(None,))
+    encoder_embed =  layers.Embedding(num_encoder_tokens, latent_dim, mask_zero=True)(encoder_inputs)
+
+    encoder_lstm = layers.LSTM(latent_dim, return_state=True)
+    encoder_outputs, state_h, state_c = encoder_lstm(encoder_embed)
+    encoder_states = [state_h, state_c]
+
+    #set up decoder
+    decoder_inputs = keras.Input(shape=(None,))
+    decoder_embed = layers.Embedding(num_decoder_tokens, latent_dim, mask_zero=True)(decoder_inputs)
+    decoder_lstm = layers.LSTM(latent_dim, return_sequences=True, return_state=True)
+    decoder_outputs, _, _  = decoder_lstm(decoder_embed, initial_state=encoder_states)
+    decoder_dense = layers.Dense(num_decoder_tokens, activation='softmax')
+    decoder_outputs= decoder_dense(decoder_outputs)
+
+    return encoder_inputs, decoder_inputs, decoder_outputs
+
+def BiDi_model(latent_dim):
+
+    #set up encoder
+    encoder_inputs = keras.Input(shape=(None,))
+    encoder_embed =  layers.Embedding(num_encoder_tokens, latent_dim, mask_zero=True)(encoder_inputs)   
+
+    encoder_lstm = layers.Bidirectional(layers.LSTM(latent_dim, return_state=True))
+    encoder_outputs, f_state_h, f_state_c, b_state_h, b_state_c = encoder_lstm(encoder_embed)
+    state_h = keras.layers.Concatenate()([f_state_h, b_state_h])
+    state_c = keras.layers.Concatenate()([f_state_c, b_state_c])
+    encoder_states = [state_h, state_c] 
+
+    #decoder
+    decoder_inputs = keras.Input(shape=(None,))
+    decoder_embed = layers.Embedding(num_decoder_tokens, latent_dim, mask_zero=True)(decoder_inputs)
+    decoder_lstm = layers.LSTM(latent_dim*2, return_sequences=True, return_state=True)
+    decoder_outputs, _, _  = decoder_lstm(decoder_embed, initial_state=encoder_states)
+    decoder_dense = layers.Dense(num_decoder_tokens, activation='softmax')
+    decoder_outputs= decoder_dense(decoder_outputs)
+
+    return encoder_inputs, decoder_inputs, decoder_outputs
+
+def GRU_model(latent_dim):
+    #set up encoder
+    encoder_inputs = keras.Input(shape=(None,))
+    encoder_embed =  layers.Embedding(num_encoder_tokens, latent_dim, mask_zero=True)(encoder_inputs)
+    encoder_gru = layers.GRU(latent_dim, return_state=True)
+    encoder_outputs, state_h = encoder_gru(encoder_embed)
+
+    #decoder
+    decoder_inputs = keras.Input(shape=(None,))
+    decoder_embed = layers.Embedding(num_decoder_tokens, latent_dim, mask_zero=True)(decoder_inputs)
+    decoder_gru = layers.GRU(latent_dim, return_sequences=True, return_state=True)
+    decoder_outputs, _ = decoder_gru(decoder_embed, initial_state=state_h)
+    decoder_dense = layers.Dense(num_decoder_tokens, activation='softmax')
+    decoder_outputs= decoder_dense(decoder_outputs)
+
+    return encoder_inputs, decoder_inputs, decoder_outputs
+
+def run_model(model_type):
 
     batch_size = 128
     epochs = 50
     latent_dim = 256
 
+    encoder_inputs = None
+    decoder_inputs = None
+    decoder_outputs = None
 
-    #set up encoder
-    encoder_inputs = keras.Input(shape=(None,))
-    encoder_embed =  layers.Embedding(num_encoder_tokens, latent_dim, mask_zero=True)(encoder_inputs)
-    
-    ####LSTM###
-    #encoder_lstm = layers.LSTM(latent_dim, return_state=True)
-    # encoder_outputs, state_h, state_c = encoder_lstm(encoder_embed)
-    #encoder_states = [state_h, state_c]
-    
-    ###Birdirectional####
-    encoder_lstm = layers.Bidirectional(layers.LSTM(latent_dim, return_state=True))
-    encoder_outputs, f_state_h, f_state_c, b_state_h, b_state_c = encoder_lstm(encoder_embed)
-    state_h = keras.layers.Concatenate()([f_state_h, b_state_h])
-    state_c = keras.layers.Concatenate()([f_state_c, b_state_c])
-    encoder_states = [state_h, state_c]
+    #get right model definition
+    if(model_type == "LSTM"):
+        encoder_inputs, decoder_inputs, decoder_outputs = LSTM_model(latent_dim)
+    elif(model_type == "BiDi"):
+        encoder_inputs, decoder_inputs, decoder_outputs = BiDi_model(latent_dim)
+    elif(model_type == "GRU"):
+        encoder_inputs, decoder_inputs, decoder_outputs = GRU_model(latent_dim)
+    else:
+        print("ERROR: invalid model defined")
 
-    ####GRU####
-    #encoder_gru = layers.GRU(latent_dim, return_state=True)
-    #encoder_outputs, state_h = encoder_gru(encoder_embed)
-    
+    if(encoder_inputs != None):
 
-    #set up decoder
-    decoder_inputs = keras.Input(shape=(None,))
-    decoder_embed = layers.Embedding(num_decoder_tokens, latent_dim, mask_zero=True)(decoder_inputs)
+        #build and train the model
+        model = keras.Model([encoder_inputs, decoder_inputs], decoder_outputs)
+        model.summary()
 
-    ####LSTM####
-    #decoder_lstm = layers.LSTM(latent_dim, return_sequences=True, return_state=True)
-    #decoder_outputs, _, _  = decoder_lstm(decoder_embed, initial_state=encoder_states)
-    
-    ####Bidirectional####
-    decoder_lstm = layers.LSTM(latent_dim*2, return_sequences=True, return_state=True)
-    decoder_outputs, _, _  = decoder_lstm(decoder_embed, initial_state=encoder_states)
+        model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['acc'])
+        #model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=[keras.metrics.CategoricalAccuracy()])
+        print(num_encoder_tokens)
+        print("fitting model:")
+        model.fit([encoder_input_vector, decoder_input_vector], decoder_output_vector, batch_size=batch_size, epochs=epochs, validation_split=0.2)
 
-    ####GRU####
-    #decoder_gru = layers.GRU(latent_dim, return_sequences=True, return_state=True)
-    #decoder_outputs, _ = decoder_gru(decoder_embed, initial_state=state_h)
-
-    decoder_dense = layers.Dense(num_decoder_tokens, activation='softmax')
-    decoder_outputs= decoder_dense(decoder_outputs)
-
-    #build and train the model
-    model = keras.Model([encoder_inputs, decoder_inputs], decoder_outputs)
-    model.summary()
-
-    model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['acc'])
-    #model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=[keras.metrics.CategoricalAccuracy()])
-    print(num_encoder_tokens)
-    print("fitting model:")
-    model.fit([encoder_input_vector, decoder_input_vector], decoder_output_vector, batch_size=batch_size, epochs=epochs, validation_split=0.2)
-
-    model.save("en2fr")
+        model.save("en2fr")
 
 
