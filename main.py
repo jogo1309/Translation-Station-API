b8a9cc4206bb079e2d2276f9f2ba5ebbe97a8c48
joshua.wood@btinternet.com <joshua.wood@btinternet.com>
2021-01-11 12:04:42 +0000

    added easier way to predict sentances

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import models.predict as predict
 from models.read_data import split_into_files
 from pathlib import Path
 
-model.run_model("GRUP")
-#predict.predict(model.input_word_index, model.output_word_index, model.max_input_length, "read this now")
+#model.run_model("GRUP")
+predict.predict("test", model.input_word_index, model.output_word_index, model.max_input_length, "i'm really cold")
 
 #split_into_files(Path(__file__).parent / 'models' / 'Data'  / 'fra.txt')
 
