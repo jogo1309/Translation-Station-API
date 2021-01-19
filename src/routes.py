f9b96d61934776c97e4f23b79bba116ace52f02d
joshua.wood@btinternet.com <joshua.wood@btinternet.com>
2021-01-19 13:11:00 +0000

    added prediction route

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,26 @@
+from src.errors.APIerror import APIError
 from .main import app
 
-@app.route('/test')
-def test():
-    return "TEst"
+
+import src.models.en_2_fr as model
+import src.models.predict as predict
+
+#from models.read_data import split_into_files
+from pathlib import Path
+
+MODEL_TYPES = ["BiDi", "LSTM", "GRU"]
+
+@app.route('/translate/<model_id>/<sentance>')
+def test(model_id, sentance):
+    if(not model_id in MODEL_TYPES):
+        raise APIError("Error: invalid model specified", 400)
+    prediction = predict.predict(model_id, model.input_word_index, model.output_word_index, model.max_input_length, sentance)
+    return {
+        "eng": sentance,
+        "fr": prediction
+    }
+
+#model.run_model("GRU")
+#predict.predict("GRU", model.input_word_index, model.output_word_index, model.max_input_length, "you've failed")
+
+#split_into_files(Path(__file__).parent / 'models' / 'Data'  / 'fra.txt')
